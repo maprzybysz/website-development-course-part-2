f70e750d5af8aee7913e4c3e4dd1fa584db35ce0
Mateusz Przybysz <mateusz.przybysz@hatomi.pl>
2021-10-04 13:54:23 +0200

    m07-js-variables

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const color = 'blue';
+let age = 24;
+const name = 'Mateusz';
+console.log(
+	'Cześć, mam na imie ' +
+		name +
+		' mam ' +
+		age +
+		' lat i mój ulubiony kolor to ' +
+		color
+);
+
+console.log(
+	`Cześć, mam na imię ${name}, mam ${age} lat i mój ulubiony kolor to ${color}`
+);
