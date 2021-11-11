a5e17150588f4da077a7642c9df2ed97525f0c14
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-11 13:04:29 +0100

    m07-arthmetic-operators

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-const name = 'Adam';
-const age = 245;
-const isTrue = true;
-const test = null;
+const add = 5 + 6;
+console.log(`wynik dodawania: ${add}`);
+
+const substract = 10 - 5;
+console.log(`Wynik odejmowania: ${substract}`);
+
+const multiply = 2 * 4;
+console.log(`Wynik mnożenia: ${multiply}`);
+
+const divide = 9 / 3;
+console.log(`Wynik dzielenia: ${divide}`);
+
+const modulo = 25 % 7;
+console.log(`Wynik modulo: ${modulo}`);
+
+let num = 4;
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num);
+--num;
+console.log(num);
