a61fa0ffe306e783a7e728d885233568aaa73c03
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-11 13:16:41 +0100

    m07-comprasion-operators

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 let x = 8;
-let y = 4;
-x = x + y;
-console.log(x);
-x += y;
-console.log(x);
-x = x - y;
-x -= y;
+let y = '8';
+let z = 9;
+
+/* 
+== sprawdzamy, czy cos jest rowne (sama wartosc)
+=== sprawdzamy wartosc + typ danych
+
+
+*/
+
+if (x != y) {
+	console.log('👍');
+} else {
+	console.log('👎');
+}
