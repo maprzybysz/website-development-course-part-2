b43208c2f27d27dc8d29113e4824895cbd319339
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-11 13:08:41 +0100

    m07-assignment-operators

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-const add = 5 + 6;
-console.log(`wynik dodawania: ${add}`);
-
-const substract = 10 - 5;
-console.log(`Wynik odejmowania: ${substract}`);
-
-const multiply = 2 * 4;
-console.log(`Wynik mnożenia: ${multiply}`);
-
-const divide = 9 / 3;
-console.log(`Wynik dzielenia: ${divide}`);
-
-const modulo = 25 % 7;
-console.log(`Wynik modulo: ${modulo}`);
-
-let num = 4;
-console.log(num);
-num++;
-console.log(num);
-num--;
-console.log(num);
---num;
-console.log(num);
+let x = 8;
+let y = 4;
+x = x + y;
+console.log(x);
+x += y;
+console.log(x);
+x = x - y;
+x -= y;
