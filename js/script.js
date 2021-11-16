0086d004242d79d956a01ab90b82c284b033d681
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-16 17:48:24 +0100

    m07-arrays

--- js/script.js
+++ js/script.js
@@ -1,9 +1,2 @@
-const passLength = 10;
-
-if (passLength > 10) {
-	console.log('Masz bardzo dobre hasło!');
-} else if (passLength >= 5 && passLength <= 10) {
-	console.log('Masz dobre hasło');
-} else {
-	console.log('Masz słabe hasło...');
-}
+const colors = ['red', 'green', 'blue'];
+console.log(colors[2]);
