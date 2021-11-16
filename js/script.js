4faa8d5e22700497c2f9c273976933d94dcc97d7
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-16 18:06:59 +0100

    m07-task3;

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-function addNumber(x, y) {
-	return x + y;
+const numbers = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < numbers.length; i++) {
+	console.log(numbers[i]);
 }
 
-function hello() {
-	console.log('Hello');
+function maths(x, y, z) {
+	console.log(x + y * z);
 }
-
-hello();
+maths(1, 2, 3);
+if (numbers.length > 5) {
+	console.log(true);
+} else {
+	console.log(false);
+}
