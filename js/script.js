56e64cd0f69709c6fa1ed0de90976c3a9dc33508
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-17 21:45:23 +0100

    m07-project-decrease-increase-change-color

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-const arrowIcon = document.querySelector('.fas');
-const btn = document.querySelector('.arrow');
-const img = document.querySelector('.item1');
+const sizeUp = document.querySelector('.size-up');
+const sizeDown = document.querySelector('.size-down');
+const color = document.querySelector('.color');
+const p = document.querySelector('p');
 
-function showImage() {
-	img.classList.toggle('show');
-
-	if (img.classList.contains('show')) {
-		arrowIcon.style.transform = 'rotate(180deg)';
-	} else {
-		arrowIcon.style.transform = 'rotate(0)';
-	}
+let fontSize = 20;
+function bigText() {
+	fontSize += 5;
+	p.style.fontSize = fontSize + 'px';
+}
+function smallText() {
+	fontSize -= 5;
+	p.style.fontSize = fontSize + 'px';
+}
+function changeText(name) {
+	p.innerHTML = name;
+}
+function changeColor() {
+	const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	p.style.color = color;
+	changeText(color);
 }
 
-btn.addEventListener('click', showImage);
+sizeUp.addEventListener('click', bigText);
+sizeDown.addEventListener('click', smallText);
+color.addEventListener('click', changeColor);
